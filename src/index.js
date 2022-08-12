a454af78a2eef8396c296db0612951e09d55bde6
msidoranka <the_quiet_place@mail.ru>
2022-08-12 22:51:51 +0300

    feat: create a function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  let resultArray = [];
+  if (!matrix || matrix.length === 0) {return [];}
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      if(matrix === 0) return [];
+      if (j === matrix.length - 1) {
+        resultArray = resultArray.concat(resultArray[i][j]);
+      }
+      else resultArray = resultArray.concat(resultArray[i][j]) + ',';
+    }
+  }
+  return resultArray;
 }
